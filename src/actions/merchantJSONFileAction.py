d68082b49f086b08f6af8493615026c63a01036a
zhengwei <zhengweiSE@gmail.com>
2016-09-26 16:38:01 +0800

    商家的文件同步完善

--- src/actions/merchantJSONFileAction.py
+++ src/actions/merchantJSONFileAction.py
@@ -1,24 +1,31 @@
 from TestPackage.FileReader import FileReaderClass
-from dbAction.MySQLConnect import MySQL, Insert
+from dbAction.MySQLConnect import MySQL, Insert, Update, UpdateOrInsert
 
-file = FileReaderClass('/Users/zhengwei/Desktop/test2.json')
+file = FileReaderClass('/Users/zhengwei/Desktop/test1.json')
 
 datas = file.eachData()
 print(len(datas))
 
-insert = Insert("INSERT INTO merchant(id,name,code,province_id,city_id,is_deleted)",
-                ['ID', 'MERCHANTNAME', 'MERCHANTCODE', 'PROVINCEID', 'CITYID', 'ISDELETED'])
-
-strsql = insert.getSQLByList(datas=datas)
-print(strsql)
-
 # 本地测试
-db = MySQL(host="127.0.0.1", user="root", passwd="root", db="eyes")
+# db = MySQL(host="127.0.0.1", user="root", passwd="root", db="eyes")
 
 # 正式数据库
 # db = MySQL(host="10.230.3.92", port=8088, user="eyes_prog", passwd="3FDEF8AED7DD6E43E3", db="eyes")
 
 # 测试数据库
-# db = MySQL(host="10.3.47.82", port=3308, user="eyes", passwd="eyes", db="eyes")
+db = MySQL(host="10.3.47.82", port=3308, user="eyes", passwd="eyes", db="eyes")
 
-re = db.cursor(strsql)
+insert = Insert("INSERT INTO merchant(id,name,code,province_id,city_id,is_deleted)",
+                ['ID', 'MERCHANTNAME', 'MERCHANTCODE', 'PROVINCEID', 'CITYID', 'ISDELETED'])
+
+update = Update(tableName="merchant",
+                set={"MERCHANTNAME": "name",
+                     "MERCHANTCODE": "code",
+                     "PROVINCEID": "province_id",
+                     "CITYID": "city_id",
+                     "ISDELETED": "is_deleted"},
+                where=" id=${ID}")
+
+UpdateOrInsert = UpdateOrInsert(insert, update, query="SELECT * FROM merchant WHERE id=${ID}", db=db)
+
+UpdateOrInsert.pushDatas(datas)
